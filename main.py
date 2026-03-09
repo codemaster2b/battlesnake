3192313ab7a0c4cbf01e5920c827b8e0f17e3e96
Jon Butterfield <codemaster2b@yahoo.com>
2026-03-09 14:07:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -89,6 +89,7 @@ def end(gameState: typing.Dict):
 # move is called on every turn and returns your next move
 def move(gameState: typing.Dict) -> typing.Dict:
     start_time = datetime.now()
+    game_id = gameState["game"]["id"]
     print_and_log(game_id, str(gameState), True)
     gameState["board"]["myId"] = gameState["you"]["id"]
     gameState["board"]["map"] = gameState["game"]["map"]
@@ -407,4 +408,5 @@ if __name__ == "__main__":
             log_file_name = sys.argv[i + 1]
 
     run_server({"info": info, "start": start, "move": move, "end": end, "port": port})
+
 
